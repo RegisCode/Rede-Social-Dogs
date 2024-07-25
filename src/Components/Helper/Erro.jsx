abeff90222cb416251211b1bd80e8511bd6b2e03
RegisCode <xbiruleix@gmail.com>
2024-07-24 23:44:40 -0300

    Initial commit

---
+++ src/Components/Helper/Erro.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Erro = ({ erro }) => {
+  if (!erro) return null;
+  return (
+    <p style={{ color: '#f31', margin: '1rem 0' }}>
+      {erro.toString()}
+    </p>
+  );
+};
+
+export default Erro;
